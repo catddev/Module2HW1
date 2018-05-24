499ff93ac22002a95f867d627e22ad84261c630e
айкеновад <@234_11>
2018-05-24 14:01:28 +0600

    Add project files.

---
+++ Module2HW1/Module2HW1.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+#include<math.h>
+
+void main()
+{
+	setlocale(LC_ALL, "Rus");
+
+
+	int n = 0;
+	printf("גגוהטעו םח: ");
+	scanf("%d", &n);
+
+
+	if (n == 1) {
+
+
+
+
+
+
+
+
+	}
